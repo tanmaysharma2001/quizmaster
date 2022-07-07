50b668054141996d71ce7e1d333af197f79147e3
Tanmay Sharma <sharmatanmay617@gmail.com>
2022-07-07 22:40:43 +0530

    Add Quiz Creation Logic

--- src/routes/quiz-create.svelte
+++ src/routes/quiz-create.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+
+  let activeID: number = -1;
+
   interface Question {
+    questionNumber: number;
+
     question: string;
 
     options: string[];
@@ -11,7 +16,120 @@
     correctResponseIndex: number;
   }
 
-  let Questions: Question[] = [];
+  export let Questions: Question[] = [];
+
+  function addQuestion() {
+    let ques: Question = {
+      questionNumber: 0,
+      question: "",
+      options: [],
+      type: "",
+      timeAlotted: 0,
+      correctResponseIndex: 0,
+    };
+
+    ques.questionNumber = Questions.length + 1;
+
+    ques.question = document.getElementById("questionTitle")?.value;
+
+    let i = 1;
+    for (; i < 5; i++) {
+      let id: string = "option" + i;
+
+      ques.options.push(document.getElementById(id)?.value);
+    }
+
+    ques.type = document.getElementById("questionType")?.value;
+
+    ques.timeAlotted = Number(document.getElementById("questionTime")?.value);
+
+    ques.correctResponseIndex =
+      document.getElementById("inputState")?.selectedIndex;
+
+    Questions = [...Questions, ques];
+  }
+
+  function displayQuestion(question: Question) {
+    if (activeID != -1) {
+      var currentElement = document
+        .getElementById("question" + activeID)
+        ?.classList.remove("active");
+    }
+
+    activeID = question.questionNumber;
+
+    document
+      .getElementById("question" + question.questionNumber)
+      ?.classList.add("active");
+
+    document.getElementById("questionTitle").value = question.question;
+    document.getElementById("option1").value = question.options[0];
+    document.getElementById("option2").value = question.options[1];
+    document.getElementById("option3").value = question.options[2];
+    document.getElementById("option4").value = question.options[3];
+
+    document.getElementById("questionType").value = question.type;
+    document.getElementById("questionTime").value =
+      question.timeAlotted.toString();
+    document.getElementById("inputState").selectedIndex =
+      question.correctResponseIndex;
+  }
+
+  function clearQuestion() {
+    if (activeID != -1) {
+      var currentElement = document
+        .getElementById("question" + activeID)
+        ?.classList.remove("active");
+      activeID = -1;
+    }
+
+    document.getElementById("questionTitle").value =
+      "Enter Your Question here...";
+    document.getElementById("option1").value = "";
+    document.getElementById("option2").value = "";
+    document.getElementById("option3").value = "";
+    document.getElementById("option4").value = "";
+
+    document.getElementById("questionType").value = "";
+    document.getElementById("questionTime").value = "";
+    document.getElementById("inputState").selectedIndex = 0;
+  }
+
+  function deleteQuestion() {
+    let index: number = activeID - 1;
+    Questions.splice(index, 1);
+    Questions = Questions;
+    activeID = -1;
+  }
+
+  function saveChanges() {
+    let index: number = activeID - 1;
+    Questions[index].question = document.getElementById("questionTitle")?.value;
+
+    let i = 1;
+    for (; i < 5; i++) {
+      let id: string = "option" + i;
+
+      Questions[index].options[i - 1] = document.getElementById(id)?.value;
+    }
+
+    Questions[index].type = document.getElementById("questionType")?.value;
+
+    Questions[index].timeAlotted = Number(
+      document.getElementById("questionTime")?.value
+    );
+
+    Questions[index].correctResponseIndex =
+      document.getElementById("inputState")?.selectedIndex;
+
+    Questions = Questions;
+  }
+
+  function clearQuestionList() {
+    Questions.splice(0, Questions.length);
+    Questions = Questions;
+    activeID = -1;
+  }
 </script>
 
 <svelte:head>
@@ -39,27 +157,41 @@
 
 <section>
   <div class="top-container">
-    
-    
-    <div class="questions-entries">
-      <h1 class="display-4">Questions List</h1>
+    <!-- Question-Entries -->
 
-      <div class="list-group">
+    <div class="question-container">
+      <div class="questions-entries">
+        <h1 class="display-4">Questions List</h1>
+
+        <div class="list-group">
+          {#each Questions as ques}
+            <button
+              on:click|preventDefault={displayQuestion(ques)}
+              id={"question" + ques.questionNumber}
+              type="button"
+              class="list-group-item list-group-item-action"
+              >Question {ques.questionNumber}</button
+            >
+          {/each}
+        </div>
+      </div>
+
+      <div class="buttons">
         <button
-          type="button"
-          class="list-group-item list-group-item-action active"
-          aria-current="true"
+          on:click|preventDefault={clearQuestionList}
+          type="submit"
+          class="btn btn-primary">Clear Questions List</button
         >
-          Question 1
-        </button>
-        <button type="button" class="list-group-item list-group-item-action"
-          >Question 2</button
-        >
+        <a href="/quiz">
+        <button type="submit" class="btn btn-primary">Start the Quiz</button>
+        </a>
       </div>
     </div>
 
+    <!-- Quiz Form -->
+
     <div class="quiz-form">
-      <form class="row g-3 question-form">
+      <form method="get" class="row g-3 question-form">
         <!-- Question Title -->
         <div class="col-12">
           <label for="questionTitle" class="form-label">Question Title</label>
@@ -107,14 +239,33 @@
           <label for="inputState" class="form-label">Correct Response:</label>
           <select id="inputState" class="form-select">
             <option selected>Choose...</option>
-            <option id="1response">1st response</option>
-            <option id="2response">2nd response</option>
-            <option id="3response">3rd response</option>
-            <option id="4response">4th response</option>
+            <option value="1" id="1response">1st response</option>
+            <option value="2" id="2response">2nd response</option>
+            <option value="3" id="3response">3rd response</option>
+            <option value="4" id="4response">4th response</option>
           </select>
         </div>
-        <div class="col-12">
-          <button type="submit" class="btn btn-primary">Add Question</button>
+        <div class="buttons-form col-12">
+          <button
+            on:click|preventDefault={addQuestion}
+            type="submit"
+            class="btn btn-primary">Add Question</button
+          >
+          <button
+            on:click|preventDefault={clearQuestion}
+            type="submit"
+            class="btn btn-primary">Clear Question</button
+          >
+          <button
+            on:click|preventDefault={deleteQuestion}
+            type="submit"
+            class="btn btn-primary">Delete This Question</button
+          >
+          <button
+            on:click|preventDefault={saveChanges}
+            type="submit"
+            class="btn btn-primary">Save Changes</button
+          >
         </div>
       </form>
     </div>
@@ -138,8 +289,26 @@
     padding: 10px;
   }
 
+  .buttons {
+    text-align: center;
+    margin: 40px;
+  }
+
+  .buttons-form {
+    text-align: center;
+  }
+
+  .question-container {
+    /* border: 1px solid; */
+    width: 530px;
+    height: 600px;
+  }
+
   .questions-entries {
-    width: 500px;
+    overflow: scroll;
+    width: 450px;
+    /* border: 1px solid; */
+    height: 500px;
   }
 
   .quiz-form {
