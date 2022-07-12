d1905ad268ce4fb1c58c5353a5a9a057c7bbe67a
Zeyad Alagamy <wezze2003@gmail.com>
2022-07-12 02:16:24 +0200

    Fixing resources

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: ['Airbnb', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['svelte3', '@typescript-eslint'],
+	settings: {
+		'import/resolver': {
+			typescript: {} // this loads <rootdir>/tsconfig.json to eslint
+		}
+	},
+	rules: {
+		'no-tabs': 0,
+		indent: [2, 'tab'],
+		'@typescript-eslint/no-var-requires': 0
+	}
+};
